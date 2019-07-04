38df71c7c5c4fce5fd944b22be25caa58fd12ffe
Ismael Medeiros <IsmaelM@lacunasoftware.com>
2019-07-04 18:43:34 -0300

    Removed bStamp unnecessary property.

--- src/main/java/com/lacunasoftware/pkiexpress/SignatureModel.java
+++ src/main/java/com/lacunasoftware/pkiexpress/SignatureModel.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SignatureModel {
 	private FileModel auditPackage = null;
-	private SignatureBStampModel bStamp = null;
 
 
 	@JsonProperty("auditPackage")
@@ -17,13 +16,5 @@ public class SignatureModel {
 	}
 	public void setAuditPackage(FileModel auditPackage) {
 		this.auditPackage = auditPackage;
-	}
-
-	@JsonProperty("bStamp")
-	public SignatureBStampModel getbStamp() {
-		return bStamp;
-	}
-	public void setbStamp(SignatureBStampModel bStamp) {
-		this.bStamp = bStamp;
 	}
 }
