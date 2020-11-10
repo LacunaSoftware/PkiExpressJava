67a9302823f31572160d39717bc243291ba19925
EmilyS <emily.souza.rs@gmail.com>
2020-11-10 19:37:44 -0300

    Added hasEncapsulatedContent to CadesSignature

--- src/main/java/com/lacunasoftware/pkiexpress/CadesSignature.java
+++ src/main/java/com/lacunasoftware/pkiexpress/CadesSignature.java
@@ -10,15 +10,21 @@ import java.util.List;
  */
 public class CadesSignature {
 	protected List<CadesSignerInfo> signers = new ArrayList<CadesSignerInfo>();
+	protected Boolean hasEncapsulatedContent;
 
 	
 	CadesSignature(CadesSignatureModel model) {
 		for (CadesSignerModel signerModel : model.getSigners()) {
 			this.signers.add(new CadesSignerInfo(signerModel));
 		}
+		this.hasEncapsulatedContent = model.getHasEncapsulatedContent();
 	}
 
 	public List<CadesSignerInfo> getSigners() {
 		return signers;
+	}
+
+	public Boolean getHasEncapsulatedContent() {
+		return hasEncapsulatedContent;
 	}
 }
